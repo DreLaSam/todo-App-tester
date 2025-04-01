c112bdbe2d2997a6090b4e51c9e7cd5b1ce1a8d7
DreLaSam <drelasam98@gmail.com>
2025-04-01 15:50:03 -0400

    first commit. the project is using Direct DOM manipulation. in the final interation we want React to define the UI as a function of the state.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+const todoForm = document.querySelector('form');
+const todoInput = document.getElementById('todo-input');
+const todoListUL = document.getElementById('todo-list');
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+let allTodos = getTodos();
+updateTodoList();
+
+todoForm.addEventListener('submit', function(e){
+  e.preventDefault(); //prevents page from reloading automatically 
+  // e is the event object.
+  addTodo();//function to add to-dos to allTodos array list. 
+})
+
+function addTodo(){
+  // this function will read the text from the input field 
+  // then it will add the text to the array. 
+  const todoText = todoInput.value.trim();
+  if (todoText.length > 0 ){
+    const todoObject = {
+      text: todoText,
+      completed: false
+    }
+    allTodos.push(todoObject); //adds todo to array. 
+    updateTodoList();
+    saveTodos();
+    todoInput.value=""; //clears text
+    }  
+
+}
+function updateTodoList(){
+  todoListUL.innerHTML = ""; // clears Unordered List. Goal is to recreate the todo list with every change.
+  allTodos.forEach((todo, todoIndex)=>{
+    //iterates through all of the items in the array.
+    const todoItem = createTodoItem(todo, todoIndex);
+    todoListUL.append(todoItem);
+  }) 
 }
 
-export default App;
+function createTodoItem(todo, todoIndex){
+  //makes input visible on page
+
+  //to create an html element use "document.createElement()"
+  const todoId = "todo-"+todoIndex;
+  const todoLI = document.createElement("li");
+  const todoText = todo.text;
+  todoLI.className="todo";
+  todoLI.innerHTML= `
+  <input type="checkbox" id="${todoId}">
+  <label class="custom-checkbox" for="${todoId}">
+      <svg fill="transparent" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z"/></svg>
+  </label>
+  <label for="${todoId}" class="todo-text"> 
+    ${todoText}
+  </label>
+  <button class="delete-button">
+      <svg fill="var(--secondary-color)" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>
+  </button> 
+
+  `
+  const deleteButton = todoLI.querySelector(".delete-button");
+  deleteButton.addEventListener("click", ()=>{
+    deleteTodoItem(todoIndex)
+  })
+
+  const checkbox = todoLI.querySelector("input");
+  checkbox.addEventListener("change", ()=>{
+    allTodos[todoIndex].completed = checkbox.checked;
+    saveTodos();
+  })
+  checkbox.checked = todo.completed;
+  return todoLI;
+}
+
+function deleteTodoItem(todoIndex){
+  allTodos = allTodos.filter((_,i)=>i !== todoIndex);
+  saveTodos();
+  updateTodoList();
+}
+
+function saveTodos(){
+  const todoJson = JSON.stringify(allTodos)
+  localStorage.setItem("todos", todoJson)
+}
+
+function getTodos(){
+  const todos = localStorage.getItem("todos") || "[]";
+  return JSON.parse(todos);
+}
+
+
